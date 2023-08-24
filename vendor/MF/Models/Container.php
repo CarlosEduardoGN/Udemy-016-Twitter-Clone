102cc5fb8d2b3e06acaeb6b905a98a0a31e3f092
Carlos Eduardo Guedes do Nascimento <nassaofsky@gmail.com>
2023-08-24 17:26:39 -0300

    first commit

---
+++ vendor/MF/Models/Container.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MF\Models;
+use App\Connection;
+
+class Container {
+
+    public static function getModel($model){
+
+        $class = "\\App\\Models\\".ucfirst($model);
+
+
+        $conn = Connection::getDb();
+
+        return new $class($conn);
+
+    }
+
+}
+
+
+
+
+?>
---
+++ vendor/MF/Models/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace MF\Models;
+
+    abstract class Model {
+        protected $db;
+
+        public function __construct (\PDO $db) {
+            $this->db = $db;
+    
+        }
+    }
+
+
+
+
+
+?>
